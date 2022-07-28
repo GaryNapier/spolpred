bc221608e025778cfb118b2a3f4e3379c633d3c1
GaryNapier <garynapier83@googlemail.com>
2022-07-28 11:08:27 +0100

    add L4 and many adjustments

--- r_scripts/l3_tree_vis.R
+++ r_scripts/l3_tree_vis.R
@@ -1,8 +1,18 @@
 
 
+rm(list=ls()) 
+
 setwd("~/Documents/spolpred/")
 
+library(ape)
+library(phytools)
+library(ggplot2)
+library(ggtree)
+library(dplyr)
+
 # Functions ----
+
+source("https://raw.githubusercontent.com/GaryNapier/Packages_functions/master/Functions.R")
 
 get_binary_cols <- function(x, col, prefix){
   
@@ -24,10 +34,36 @@ get_binary_cols <- function(x, col, prefix){
   # SRR2100324 1110000111111111111111000000000000011111111  1  1  1  0  0  0  0  1  1   1 ...etc
   # SRR2100410 1100000111111111111000000000000000011111111  1  1  0  0  0  0  0  1  1   1 ...etc
   
+  library(stringr)
+  
   bin_split <- str_split(x[, col], "")
   bin_mat <- data.frame(do.call("rbind", bin_split))
   names(bin_mat) <- paste0(prefix, 1:ncol(bin_mat))
   cbind(x, bin_mat)
+}
+
+ggtree_strip_cols <- function(x, alpha = 0.7){
+  
+  # Dataframe:
+  #            lin_level_1
+  # SRR8651556           2
+  # SRR8651557           4
+  # SRR8651558           2
+  # SRR8651559           2
+  # SRR8651560           2
+  # SRR8651561           4
+  
+  # Output:
+  # 1           2           3           4           5           6           7           9           M.bovis     M.caprae    M.orygis 
+  # "#FF0000E6" "#FF8B00E6" "#E8FF00E6" "#5DFF00E6" "#00FF2EE6" "#00FFB9E6" "#00B9FFE6" "#002EFFE6" "#5D00FFE6" "#E800FFE6" "#FF008BE6" 
+  
+  # One distinct colour per unique value in the dataframe. Colour names are the unique values.
+  # NOTE: Need to call the function with the column specified (i.e. read in as a vector):
+  # ggtree_strip_cols(df$<col_name>)
+  
+  cols <- rainbow(length(unique(x)), alpha = alpha)
+  names(cols) <- c(sort(unique(x)))
+  cols
 }
 
 # Paths ----
@@ -39,22 +75,35 @@ itol_templates_path <- "../pipeline/itol_templates/"
 
 # Files ----
 
-spoligo_lineage_full_file <- paste0(data_path, "spoligo_lineage.full.txt")
+# spoligo_lineage_full_file <- paste0(data_path, "spoligo_lineage.full.txt")
+spoligo_lineage_full_file <- paste0(data_path, "spoligo_lineage.full.correct_samps.csv")
 l3_treefile <- paste0(newick_path, "grouped_samples_lineage3.treefile")
+l4_treefile <- paste0(newick_path, "grouped_samples_lineage4.treefile")
+l1_7_treefile <- paste0(newick_path, "grouped_samples_L_1_7.treefile")
 # l3_treefile <- paste0(newick_path, "lin_3_all_samps.treefile")
 itol_binary_template_file <- paste0(itol_templates_path, "itol.binary.txt")
 itol_outfile <- paste0(results_path, "itol_all_spol_binary.txt")
 
 # Read in data ----
-spoligo_lineage_full <- read.table(spoligo_lineage_full_file, header = T, sep = "\t", 
+# spoligo_lineage_full <- read.table(spoligo_lineage_full_file, header = T, sep = "\t", 
+#                                    colClasses = c("spoligotype" = "character"))
+
+
+
+spoligo_lineage_full <- read.csv(spoligo_lineage_full_file, header = T, 
                                    colClasses = c("spoligotype" = "character"))
 l3_tree <- midpoint.root(read.tree(l3_treefile))
+l4_tree <- midpoint.root(read.tree(l4_treefile))
+l1_7_tree <- midpoint.root(read.tree(l1_7_treefile))
 itol_binary_template <- readChar(itol_binary_template_file, nchars = 1e6)
 
 
 # Clean ----
 
 # l3_samples <- l3_tree$tip.label
+
+# Remove blank lineages
+spoligo_lineage_full <- subset(spoligo_lineage_full, !(lineage == ""))
 
 # Remove "lineage"
 spoligo_lineage_full$lineage <- gsub("lineage", "", spoligo_lineage_full$lineage)
@@ -63,37 +112,139 @@ spoligo_lineage_full$lineage <- gsub("lineage", "", spoligo_lineage_full$lineage
 animal <- subset(spoligo_lineage_full, grepl("M", lineage))
 spoligo_lineage_full <- subset(spoligo_lineage_full, !(grepl("M", lineage)))
 
-# Expand lineages and merge
-exp <- expand_hierarchy(spoligo_lineage_full, "sample", "lineage")
+# Expand lineages, rbind animal back in and merge
+# exp <- expand_hierarchy(spoligo_lineage_full, "sample", "lineage")
+exp <- expand_hierarchy_fill(spoligo_lineage_full, "sample", "lineage")
 data <- merge(rbind(spoligo_lineage_full, animal), exp,
               by.x = "sample", by.y = "id", all.x = T, 
               sort = F)
 
+# Fill in cols lin_level_1 and max_lin for animal
 data$lin_level_1 <- ifelse(grepl("M", data$lineage), data$lineage, data$lin_level_1)
+data$lin_level_2 <- ifelse(grepl("M", data$lineage), data$lineage, data$lin_level_2)
+data$lin_level_3 <- ifelse(grepl("M", data$lineage), data$lineage, data$lin_level_3)
+data$lin_level_4 <- ifelse(grepl("M", data$lineage), data$lineage, data$lin_level_4)
+data$lin_level_5 <- ifelse(grepl("M", data$lineage), data$lineage, data$lin_level_5)
 data$max_lin <- ifelse(grepl("M", data$lineage), data$lineage, data$max_lin)
 
-# Remove 0000000000000000000000000000000000000000000 ? 
+# Remove 0000000000000000000000000000000000000000000 - error apparently
 data <- subset(data, !(spoligotype == "0000000000000000000000000000000000000000000"))
 
 # Remove everything except lin and spoligotype info
-data <- dplyr::select(data, sample:lineage, lin_level_1:max_lin)
+# data <- dplyr::select(data, sample:lineage, lin_level_1:max_lin)
 
 # Keep only those with 5 or more samples per spoligo
-data <- data %>% group_by(spoligotype) %>% filter(n() > 4) %>% data.frame()
+# data <- data %>% group_by(spoligotype) %>% filter(n() > 4) %>% data.frame()
+# nrow(data)
+
 # Add rownames for plotting
 rownames(data) <- data$sample
 
-# Subset L3 
-# l3_spol_data <- subset(data, sample %in% l3_samples)
-# Add binary cols
+# Add binary cols - convert binary spoligotype 00101 etc to columns - for both itol and ggtree
 spol_data <- get_binary_cols(data, "spoligotype", "s")
-# subset spol data 
-spol_data_ggplot <- select(spol_data, s1:s43)
+
+# ggtree ----
+
+# subset data by relevant cols - i.e. remove lineage and original spoligotype cols for ggplot
+lin_lv1_data_ggplot <- select(spol_data, lin_level_1)
+lin_lv2_data_ggplot <- select(spol_data, lin_level_2)
+spol_data_ggplot <- select(spol_data, -(sample:max_lin))
+
+# Colours
+
+lin_colours_lv_1 <- ggtree_strip_cols(lin_lv1_data_ggplot$lin_level_1)
+
+# Split out data by lineage level 1
+spol_data_lv1_split <- split(spol_data, spol_data$lin_level_1)
+# Add 'lineage' for easier subsetting
+names(spol_data_lv1_split) <- paste0("lineage", names(spol_data_lv1_split))
+
+# Get colours fro each level 2 (4.1, 4.2 etc)
+l3_lv2_cols <- ggtree_strip_cols(spol_data_lv1_split$lineage3$lin_level_2)
+l4_lv2_cols <- ggtree_strip_cols(spol_data_lv1_split$lineage4$lin_level_2)
+
+# Set up ggtree parameters 
+width <- 0.05
+font_sz <- 3
+line_sz <- 0.1
+angle <- 30
+os <- 0.0010
+
+# L3
+
+l3_ggtree <- ggtree(l3_tree, size = line_sz, layout = "circular")
+l3_ggtree <- gheatmap(l3_ggtree, lin_lv2_data_ggplot,
+                      color = NA, 
+                      width = width,
+                      offset = 0,
+                      colnames = F)+
+  scale_fill_manual(values = l3_lv2_cols, breaks = names(l3_lv2_cols) )+
+  labs(fill = "Subineage")
+
+l3_ggtree <- l3_ggtree + ggnewscale::new_scale_fill()
+
+l3_ggtree <- gheatmap(l3_ggtree, spol_data_ggplot,
+         offset = os+0.0020,
+         color = NA,
+         low="white",
+         high="black",
+         colnames = F) +
+  scale_fill_manual(values=c("white", "black"), labels = c("0", "1", "NA"), na.value = "grey")+
+  labs(fill = "Spoligotype")+
+  ggtitle("Lineage 3")
+
+ggsave(file = paste0(results_path, "lin3_ggtree.png"), plot = l3_ggtree)
+ggsave(file = paste0(results_path, "lin3_ggtree.svg"), plot = l3_ggtree)
+
+
+# L4
+
+# Subset L4, still too big
+set.seed(123)
+l4_tree <- keep.tip(l4_tree, sample(l4_tree$tip.label, floor(length(l4_tree$tip.label)*0.5)))
+
+l4_ggtree <- ggtree(l4_tree, size = line_sz, layout = "circular")
+l4_ggtree <- gheatmap(l4_ggtree, lin_lv2_data_ggplot,
+                      color = NA,
+                      width = 0.2,
+                      offset = 0,
+                      colnames = F)+
+  scale_fill_manual(values = l4_lv2_cols, breaks = names(l4_lv2_cols) )+
+  labs(fill = "Subineage")
+
+l4_ggtree <- l4_ggtree + ggnewscale::new_scale_fill()
+
+l4_ggtree <- gheatmap(l4_ggtree, spol_data_ggplot,
+                      offset = os+0.0020,
+                      color = NA,
+                      low="white",
+                      high="black",
+                      colnames = F) +
+  scale_fill_manual(values=c("white", "black"), labels = c("0", "1", "NA"), na.value = "grey")+
+  labs(fill = "Spoligotype")+
+  ggtitle("Lineage 4")
+
+ggsave(file = paste0(results_path, "lin4_ggtree.png"), plot = l4_ggtree)
+ggsave(file = paste0(results_path, "lin4_ggtree.svg"), plot = l4_ggtree)
+
+
+# L1 & L7
+
+l1_7_ggtree <- ggtree(l1_7_tree, size = line_sz, layout = "circular")
+
+l1_7_ggtree <- gheatmap(l1_7_ggtree, lin_lv1_data_ggplot,
+                        color = NA,
+                        width = 0.2,
+                        offset = 0,
+                        colnames = F, 
+                        legend_title = "Lineage")+
+  scale_fill_manual(values = lin_colours_lv_1, breaks = names(lin_colours_lv_1) )
 
 
 # itol ----
 
-itol_data <- select(spol_data, sample, s1:s43)
+# Subset for itol
+itol_data <- select(spol_data, -(spoligotype:max_lin), sample)
 len_spol <- len_str("1110000000111111111111000000000000011111000")
 
 # FIELD_SHAPES,1,1,1
@@ -119,32 +270,15 @@ write.table(itol_data, file = itol_outfile,
 
 
 
-# Set up ggtree parameters ----
 
-width <- 0.05
-font_sz <- 3
-line_sz <- 0.25
-angle <- 30
 
-tree <- ggtree(l3_tree, size = line_sz, layout = "circular")
 
-gheatmap(tree, spol_data_ggplot,
-         # Increase offset
-         # offset = offset+200,
-         # width = width+0.5,
-         # Change color to black
-         # color = NULL,
-         color="black",
-         low="white",
-         high="black",
-         colnames_position = "top",
-         # colnames_angle = angle,
-         colnames_offset_y = 1,
-         hjust = 0,
-         font.size = 2.5) +
-  # Define colours
-  scale_fill_manual(values=c("white", "black"), labels = c("0", "1", "NA"), na.value = "grey")+
-  labs(fill = "Spoligotype")
+
+
+
+
+
+
 
 
 
